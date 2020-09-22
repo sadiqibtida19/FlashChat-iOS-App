eda59a38d25dc2bac7866f64887283b7d42ff555
sadiqibtida19 <71648926+sadiqibtida19@users.noreply.github.com>
2020-09-23 05:07:08 +0800

    Added firebase feature
    User login and logout

---
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ChatViewController.swift
+//  Flash Chat iOS13
+//
+//  Created by Angela Yu on 21/10/2019.
+//  Copyright © 2019 Angela Yu. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class ChatViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var messageTextfield: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "⚡️FlashChat"
+        navigationItem.hidesBackButton = true
+
+    }
+    
+    @IBAction func sendPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func logOutPressed(_ sender: UIBarButtonItem) {
+        let firebaseAuth = Auth.auth()
+        do {
+            try firebaseAuth.signOut()
+            navigationController?.popToRootViewController(animated: true)
+            } catch let signOutError as NSError {
+                print ("Error signing out: %@", signOutError)
+            }
+    }
+    
+    
+}
+
